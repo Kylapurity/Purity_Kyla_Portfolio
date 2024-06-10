0a983b0810bfe76153e5200158214191336ec899
pkihiu <p.kihiu@alustudent.com>
2024-06-10 12:55:11 +0300

    update Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
   
   return (
     <footer>
-      <a href="#home" className="footer__logo">Moyo-Weke</a>
+      <a href="#home" className="footer__logo">Purity Kihiu</a>
       <ul className="permalinks">
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
