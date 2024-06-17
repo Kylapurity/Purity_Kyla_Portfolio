a95eadba971473d299d7ab6b9632a7d899d6d822
pkihiu <p.kihiu@alustudent.com>
2024-06-17 13:56:53 +0300

    update portfoil

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import IMG1 from "../../assets/social-media.png";
-import IMG2 from "../../assets/skilled.png";
-import IMG3 from "../../assets/interac.png";
-import IMG4 from "../../assets/social-proof.jpg";
-import IMG5 from "../../assets/edge.png";
-import IMG6 from "../../assets/roomify.png";
-import IMG7 from "../../assets/graspa.png";
-import IMG8 from "../../assets/credence.png";
-import IMG9 from "../../assets/hilto.png";
+import Resto from "../../assets/Resto.jpeg";
+import Creative from "../../assets/Creative.jpeg";
+import Healthpass from "../../assets/Healthpass.jpeg";
+import Math from "../../assets/Math.jpeg";
+import Travel from "../../assets/travel.jpeg";
+import We_Care from "../../assets/We-Care.jpeg";
+import event from "../../assets/event.jpeg";
+import Airbnb from "../../assets/AirBnb.jpeg";
+import Bussiness from "../../assets/Bussiness.jpeg";
 
 import "./portfolio.css";
 import { useState } from "react";
@@ -18,117 +18,108 @@ const Portfolio = () => {
   const [filteredProjects, setFilteredProjects] = useState([]);
   const soloProjects = [
     {
-      id: 7,
-      title: "Graspa",
-      img: IMG7,
-      category: "Design",
+      id: 1,
+      title: "Resturant",
+      img: Resto,
+      category: "Steak",
       description:
         "Graspa is a cloud-based learning management system designed to provide students with an online learning environment that allows them to access educational content and resources",
-      technologies: "UI Design | UI/UX Research",
-      figmalink:
-        "https://www.figma.com/proto/WbET2pgVnHu8t9J5KlBTK2/GRASPA-E-LEARNING-PLATFORM?page-id=0%3A1&node-id=48-2&viewport=227%2C229%2C0.03&scaling=scale-down&starting-point-node-id=48%3A2&show-proto-sidebar=1",
-
-      uiresearch:
-        "https://drive.google.com/file/d/1XLjKT29_EFT9DgnUNy5LmDDSE3Gg9IXK/view?usp=share_link",
+      technologies: "React || Tailwind Css",
+      link:
+        "https://github.com/Kylapurity/Restaurant-Web-React-V2",
     },
     {
-      id: 8,
-      title: "Credence",
-      img: IMG8,
-      category: "Mobile",
+      id: 2,
+      title: "Creative",
+      img: Creative,
+      category: "Desktop",
 
       description:
         "Credence is an eco-friendly mobile application that offers small businesses a platform to showcase their products and services, and facilitates sales through the generation of digital receipts instead of paper receipts.",
-      technologies: "React Native | UI/UX Research | Mobile | Agile ",
-      github: "https://github.com/Moyo-tech/Credence",
+      technologies: " UI/UX Research",
       figmalink:
-        "https://www.figma.com/proto/Q8F1P3E4atctPsLj3hVpH9/CREDENCE?page-id=0%3A1&node-id=69-15&viewport=855%2C348%2C0.25&scaling=scale-down&starting-point-node-id=69%3A15&show-proto-sidebar=1",
+        "https://www.figma.com/design/XIfdSqCPXf7NMHQEjQZBie/First-Class?node-id=0-1&t=BwChNGBvGL1w0Upo-1",
     },
     {
-      id: 9,
-      title: "Hilto",
-      img: IMG9,
-      category: "Mobile",
+      id: 3,
+      title: "Healthpass",
+      img: Healthpass,
+      category: "Desktop",
 
       description:
         "The Hospital Booking App (Hilto) is a user-friendly platform designed to streamline the process of booking appointments with healthcare providers.",
       technologies: "Flutter | Mobile | Agile ",
       figmalink:
-        "https://www.figma.com/file/6ehQVTaJVACpkkNMZaa2Dq/Patient?node-id=0%3A1&t=dVyv4A3vF5xYA0zq-1",
-      github: "https://github.com/mzbhewtee/Mini_project2",
+        "https://www.figma.com/design/RjtjtScTI3MmGPJrg22J4F/Health-Pas-Project?node-id=0-1&t=dZ2x3YaYEbJpdPKp-1",
+      github: "https://github.com/Kylapurity/BSE-FOUNDATIONS-PROJECT-HEALTH-PASS",
     },
     {
-      id: 1,
-      title: "Social Media Dashboard with Theme Switcher",
-      img: IMG1,
+      id: 4,
+      title: "Math Dashboard",
+      img: Math,
       category: "Web",
 
       description:
         "View analytics for multiple social media platforms (Instagram, LinkedIn, Youtube, Twitter) on this dashboard. Enjoy dark and light modes with animations for a better experience.",
-      technologies: "HTML | CSS | JS | Figma",
-      link: "https://moonlit-biscochitos-c99e7f.netlify.app/",
-      github: "https://github.com/Moyo-tech/social_media_dashboard",
+      technologies: "HTML | CSS | JS | React | Tailwindcss",
+      LiveDemo: "https://hyggex-app-chi.vercel.app/",
+      github: "https://github.com/Kylapurity/Hyggex-App",
     },
     {
-      id: 2,
-      title: "Skilled E-learning Platform Landing Page",
-      img: IMG2,
+      id: 5,
+      title: "Travel App",
+      img: Travel,
       category: "Web",
 
       description:
         "Landing Page for the Skilled E-learning Platform, built from the UI/UX wireframe to create a seamless experience",
-      technologies: "HTML | CSS | Figma | Boostrap",
-      link: "https://neon-cassata-ef3438.netlify.app/",
-      github: "https://github.moyo-tech/skilled_elearning_landing_page",
+      technologies: "HTML | CSS | Figma | Boostrap | React | Tailwind Css",
+      github: "https://github.com/Kylapurity/Travel-App",
     },
     {
-      id: 3,
-      title: "Interactive Rating Component",
-      category: "Web",
+      id: 6,
+      title: "Business",
+      category: "Figma",
 
-      img: IMG3,
+      img: Bussiness,
       description:
         "User friendly app to for users to rate products and services",
-      technologies: "JavaScript | HTML | CSS | Figma",
-      link: "https://effervescent-churros-4169b2.netlify.app/",
-      github: "https://github.com/Moyo-tech/Interactive-rating-project",
+      technologies: "Figma",
+      figma: "https://www.figma.com/design/5xXiFlTd3K4KnM4kQBcHrY/Design-class?node-id=0-1&t=NZ2OgPIYB3fbvLTU-1",
     },
     {
-      id: 4,
-      title: "Landing Page Social Proof",
-      img: IMG4,
+      id: 7,
+      title: "AirBnB",
+      img: Airbnb,
       category: "Web",
       description:
         "Fully responsive website that shows the testimonials of a business, built based on Figma design ",
-      technologies: "CSS Grid | HTML ",
-      link: "https://prismatic-palmier-e36078.netlify.app/",
-      github: "https://github.com/Moyo-tech/frontend-social-proof-section",
+      technologies: "CSS Grid | HTML | Python ",
+      github: "https://github.com/Kylapurity/AirBnB_clone_v2",
     },
     {
-      id: 5,
-      title: "Edge-Ledger-Landing-Page",
-      img: IMG5,
+      id: 8,
+      title: "Women E-Learning",
+      img: We_Care,
       category: "Web",
 
       description:
         "Fully responsive interactive website Landing Page for an Ecommerce business built based on Adobe XD design",
-      technologies: "HTML | CSS",
-      link: "https://gleaming-quokka-96d2cb.netlify.app/",
-      github: "https://github.com/Moyo-tech/Edge-Ledger-Landing-Page",
+      technologies: "HTML | CSS | React | Tailwind Css",
+      Demolink: "https://we-care-sage.vercel.app/",
+      github: "https://github.com/Kylapurity/We-Care",
     },
     {
-      id: 6,
-      title: "Roomify",
-      img: IMG6,
+      id: 9,
+      title: "Event",
+      img: event,
       category: "Design",
 
       description:
         "Roomify - A mobile application that help users find roommates and rooms by creating a verified profile, setting preferences and listing advertisements.",
       technologies: "Figma | UI/UX Research",
       figmalink:
-        "https://www.figma.com/proto/X0cYmzIshFlwQOr4luwBu4/ROOMIFY-HIGH-FIDELITY-APP?page-id=0%3A1&node-id=2-76&viewport=16%2C119%2C0.11&scaling=scale-down&starting-point-node-id=2%3A76&show-proto-sidebar=1",
-      uiresearch:
-        "https://drive.google.com/file/d/1FsD9LyBSXCzRbCIEH4zYf6W98hRcbIwx/view?usp=share_link",
+        "https://www.figma.com/design/L6ykMjzjVTkZYZTUsUAoqq/Event-Manager?node-id=0-1&t=TnuzkcOuC5fnKaNK-1",
     },
   ];
   const handleCategoryClick = (newCategory) => {
